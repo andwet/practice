df080d7a41a44643ed3e82e33482efbc15b62f01
Andrew <andrew.cmsmasters.com>
2019-01-19 01:27:52 +0200

    third commit

---
+++ generator css/1.js
@@ -0,0 +1,13 @@
+document.getElementById('r1').oninput = cssGenerator;
+
+function cssGenerator() {
+	// прямоугольник
+	var div = document.getElementById('test');
+	// получаю textarea
+	var out = document.getElementById('out');
+	// console.log(this.value);
+	div.style.borderRadius = this.value + 'px';
+	// border-radius: 30px;
+	out.innerHTML = '-webkit-border-radius: ' + this.value + 'px;\n'; 
+	out.innerHTML += 'border-radius: ' + this.value + 'px;';
+}
